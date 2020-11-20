91a4919c3b620f2ccd7c5936897f4c2da5e518cb
Joe <overtonje@gmail.com>
2020-11-20 11:01:07 -0600

    phase 1

--- Program.cs
+++ Program.cs
@@ -7,6 +7,30 @@ namespace heist
         static void Main(string[] args)
         {
             Console.WriteLine("Plan your Heist!");
+
+            Console.WriteLine("Enter a Team member's name");
+            Member NumberOne = new Member(Console.ReadLine());
+            Console.WriteLine($"enter {NumberOne.Name}'s skill level");
+            NumberOne.SkillLevel = Int32.Parse(Console.ReadLine());
+            Console.WriteLine($"enter {NumberOne.Name}'s courage factor");
+            NumberOne.CourageFactor = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Your Team");
+            Console.WriteLine("----------");
+            Console.WriteLine(NumberOne.Name);
+            Console.WriteLine($"Skill Level: {NumberOne.SkillLevel}");
+            Console.WriteLine($"Courage Factor: {NumberOne.CourageFactor}");
+        }
+
+        class Member
+        {
+            public string Name { get; set; }
+            public int SkillLevel { get; set; }
+            public double CourageFactor { get; set; }
+            public Member(string name)
+            {
+                Name = name;
+            }
         }
     }
 }
